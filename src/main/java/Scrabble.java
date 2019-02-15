941aec33e569e3b62d5a3343dca447013e75aace
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-02-15 13:54:37 +0000

    feat: added code to calcultate lowercase letter word scores

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,11 +1,25 @@
+import java.util.*;
 class Scrabble {
 
+  private int score = 0;
+  private static final String[] allLetters = {"aeioulnrst", "dg", "bcmp", "fhvwy", "k", "jx", "qz"};
+  private static final int scores[] = {1,2,3,4,5,8,10};
+  Map<Character, Integer> mapLetterScores = new HashMap<Character, Integer>();
+
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      for (int i = 0; i < allLetters.length; i++) {
+        char [] allLettersToChars = allLetters[i].toCharArray();
+        for (int j = 0; j < allLettersToChars.length; j++) {
+          mapLetterScores.put(allLettersToChars[j], scores[i]);
+        }
+      }
+      char [] letterToChars = word.toCharArray();
+      for (int k = 0; k < letterToChars.length; k++) {
+        this.score += mapLetterScores.get(letterToChars[k]);
+      }
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return score;
     }
-
 }
--- src/test/java/ScrabbleScoreTest.java
+++ src/test/java/ScrabbleScoreTest.java
@@ -18,35 +18,35 @@ public class ScrabbleScoreTest {
         assertEquals(1, scrabble.getScore());
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void testAValuableLetter() {
         Scrabble scrabble = new Scrabble("f");
         assertEquals(4, scrabble.getScore());
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void testAShortWord() {
         Scrabble scrabble = new Scrabble("at");
         assertEquals(2, scrabble.getScore());
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void testAShortValuableWord() {
         Scrabble scrabble = new Scrabble("zoo");
         assertEquals(12, scrabble.getScore());
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void testAMediumWord() {
         Scrabble scrabble = new Scrabble("street");
         assertEquals(6, scrabble.getScore());
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void testAMediumValuableWord() {
         Scrabble scrabble = new Scrabble("quirky");
@@ -60,7 +60,7 @@ public class ScrabbleScoreTest {
         assertEquals(41, scrabble.getScore());
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void testAEnglishLikeWord() {
         Scrabble scrabble = new Scrabble("pinata");
@@ -74,7 +74,7 @@ public class ScrabbleScoreTest {
         assertEquals(0, scrabble.getScore());
     }
 
-    @Ignore("Remove to run test")
+    // @Ignore("Remove to run test")
     @Test
     public void testEntireAlphabetAvailable() {
         Scrabble scrabble = new Scrabble("abcdefghijklmnopqrstuvwxyz");
